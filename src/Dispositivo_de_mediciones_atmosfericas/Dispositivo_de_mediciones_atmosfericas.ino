94504571aaac255f1803a1af9801a6359314a1c9
Pygramer78 <ayuskyap@gmail.com>
2025-05-16 21:56:47 +0200

    Links

--- src/Dispositivo_de_mediciones_atmosfericas/Dispositivo_de_mediciones_atmosfericas.ino
+++ src/Dispositivo_de_mediciones_atmosfericas/Dispositivo_de_mediciones_atmosfericas.ino
@@ -5,6 +5,15 @@ Está especialmente usado para la presión, temperatura, humedad, x.
 De Pygramer78[Álvaro](yo) y Sergio-dr(tú)
 ***********************************************************************************/
 
+/***********************************************
+Links de repositorios github para librerías:
+=====================================================================================
+AHT20: https://github.com/dvarrel/AHT20
+TSL2591: https://github.com/adafruit/Adafruit_TSL2591_Library
+BMP280: https://github.com/adafruit/Adafruit_BMP280_Library
+MLX90615: https://github.com/Seeed-Studio/Digital_Infrared_Temperature_Sensor_MLX90615
+======================================================================================
+***********************************************/
 
 #include <AHT20.h>
 #include <Adafruit_BMP280.h>
