d387e399e705ca8ca0bf13fde7c77abc6a942590
Sergio <sdiaz@us.es>
2025-05-17 20:08:30 +0200

    Primera revisión

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Dispositivo-atmosferico
 
+Father&Son project!!!
+
 ## Sensores
 - Temperatura/humedad: AHT20
-- Presión: BMP280
-- Luminosidad: TSL2591
-- Temperatura infrarroja: MLX90615
+- Presión atmosférica: BMP280
+- Brillo de cielo nocturno: TSL2591
+- Temperatura infrarroja para detección de nubes: MLX90615
--- src/Dispositivo_de_mediciones_atmosfericas/Dispositivo_de_mediciones_atmosfericas.ino
+++ src/Dispositivo_de_mediciones_atmosfericas/Dispositivo_de_mediciones_atmosfericas.ino
@@ -5,59 +5,56 @@ Está especialmente usado para la presión, temperatura, humedad, x.
 De Pygramer78[Álvaro](yo) y Sergio-dr(tú)
 ***********************************************************************************/
 
-/***********************************************
-Links de repositorios github para librerías:
-=====================================================================================
-AHT20: https://github.com/dvarrel/AHT20
-TSL2591: https://github.com/adafruit/Adafruit_TSL2591_Library
-BMP280: https://github.com/adafruit/Adafruit_BMP280_Library
-MLX90615: https://github.com/Seeed-Studio/Digital_Infrared_Temperature_Sensor_MLX90615
-======================================================================================
-***********************************************/
-#include <Wire.h> // <-- Probablemente haga falta (sale en el ejemplo de AHT20)
+/* Links de repositorios github para librerías:
+  AHT20: https://github.com/dvarrel/AHT20
+  TSL2591: https://github.com/adafruit/Adafruit_TSL2591_Library
+  BMP280: https://github.com/adafruit/Adafruit_BMP280_Library
+  MLX90615: https://github.com/Seeed-Studio/Digital_Infrared_Temperature_Sensor_MLX90615
+*/
+#include <Wire.h>
 #include <AHT20.h>
 #include <Adafruit_BMP280.h>
-#include <Adafruit_TSL2591.h> // <-- 
-#include <MLX90615.h> // <-- infrarrojo
+#include <Adafruit_TSL2591.h>
+#include <MLX90615.h>
+
 AHT20 aht20;
-Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591); // El número aparentemente es el pin que hay que poner
-Adafruit_BMP280 bmp; // I2C
-//Adafruit_BMP280 bmp(BMP_CS); // hardware SPI
-//Adafruit_BMP280 bmp(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
-/**************************************************************************/
-/*
-    Displays some basic information on this sensor from the unified
-    sensor API sensor_t type (see Adafruit_Sensor for more information)
-*/
-/**************************************************************************/
-void displaySensorDetails(void)
-{
+Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591);  // sensorID=2591
+Adafruit_BMP280 bmp;
+// TODO: MLX90615
+
+
+/*__/ TSL2591 \________________*/
+
+// Muestra información sobre el sensor
+void TSL2591_displayDetails(void) {
   sensor_t sensor;
   tsl.getSensor(&sensor);
-  Serial.println(F("------------------------------------"));
-  Serial.print  (F("Sensor:       ")); Serial.println(sensor.name);
-  Serial.print  (F("Driver Ver:   ")); Serial.println(sensor.version);
-  Serial.print  (F("Unique ID:    ")); Serial.println(sensor.sensor_id);
-  Serial.print  (F("Max Value:    ")); Serial.print(sensor.max_value); Serial.println(F(" lux"));
-  Serial.print  (F("Min Value:    ")); Serial.print(sensor.min_value); Serial.println(F(" lux"));
-  Serial.print  (F("Resolution:   ")); Serial.print(sensor.resolution, 4); Serial.println(F(" lux"));  
-  Serial.println(F("------------------------------------"));
+  Serial.print(F("Sensor:       "));
+  Serial.println(sensor.name);
+  Serial.print(F("Driver Ver:   "));
+  Serial.println(sensor.version);
+  Serial.print(F("Unique ID:    "));
+  Serial.println(sensor.sensor_id);
+  Serial.print(F("Max Value:    "));
+  Serial.print(sensor.max_value);
+  Serial.println(F(" lux"));
+  Serial.print(F("Min Value:    "));
+  Serial.print(sensor.min_value);
+  Serial.println(F(" lux"));
+  Serial.print(F("Resolution:   "));
+  Serial.print(sensor.resolution, 4);
+  Serial.println(F(" lux"));
   Serial.println(F(""));
-  delay(500);
 }
 
-/**************************************************************************/
-/*
-    Configures the gain and integration time for the TSL2591
-*/
-/**************************************************************************/
-void configureSensor(void)
-{
+// Configura ganancia y exposición
+// TODO determinar configuración óptima para medición de brillo de cielo nocturno
+void TSL2591_configure(void) {
   // You can change the gain on the fly, to adapt to brighter/dimmer light situations
   //tsl.setGain(TSL2591_GAIN_LOW);    // 1x gain (bright light)
-  tsl.setGain(TSL2591_GAIN_MED);      // 25x gain
+  tsl.setGain(TSL2591_GAIN_MED);  // 25x gain
   //tsl.setGain(TSL2591_GAIN_HIGH);   // 428x gain
-  
+
   // Changing the integration time gives you a longer time over which to sense light
   // longer timelines are slower, but are good in very low light situtations!
   //tsl.setTiming(TSL2591_INTEGRATIONTIME_100MS);  // shortest integration time (bright light)
@@ -67,12 +64,10 @@ void configureSensor(void)
   // tsl.setTiming(TSL2591_INTEGRATIONTIME_500MS);
   // tsl.setTiming(TSL2591_INTEGRATIONTIME_600MS);  // longest integration time (dim light)
 
-  /* Display the gain and integration time for reference sake */  
-  Serial.println(F("------------------------------------"));
-  Serial.print  (F("Gain:         "));
+  /* Display the gain and integration time for reference sake */
+  Serial.print(F("Gain:         "));
   tsl2591Gain_t gain = tsl.getGain();
-  switch(gain)
-  {
+  switch (gain) {
     case TSL2591_GAIN_LOW:
       Serial.println(F("1x (Low)"));
       break;
@@ -86,161 +81,87 @@ void configureSensor(void)
       Serial.println(F("9876x (Max)"));
       break;
   }
-  Serial.print  (F("Timing:       "));
-  Serial.print((tsl.getTiming() + 1) * 100, DEC); 
+  Serial.print(F("Timing:       "));
+  Serial.print((tsl.getTiming() + 1) * 100, DEC);
   Serial.println(F(" ms"));
-  Serial.println(F("------------------------------------"));
   Serial.println(F(""));
 }
 
-
-/**************************************************************************/
-/*
-    Program entry point for the Arduino sketch
-*/
-/**************************************************************************/
-void setup(void) 
-{
-  Serial.begin(9600);
-  
-  Serial.println(F("Starting Adafruit TSL2591 Test!"));
-  
-  if (tsl.begin()) 
-  {
-    Serial.println(F("Found a TSL2591 sensor"));
-  } 
-  else 
-  {
-    Serial.println(F("No sensor found ... check your wiring?"));
-    while (1);
-  }
-    
-  /* Display some basic information on this sensor */
-  displaySensorDetails();
-  
-  /* Configure the sensor */
-  configureSensor();
-
-  // Now we're ready to get readings ... move on to loop()!
-}
-/*********************************TSL2591*Functions***************************************/
-/**************************************************************************/
-/*
-    Shows how to perform a basic read on visible, full spectrum or
-    infrared light (returns raw 16-bit ADC values)
-*/
-/**************************************************************************/
-void simpleRead(void)
-{
-  // Simple data read example. Just read the infrared, fullspecrtrum diode 
-  // or 'visible' (difference between the two) channels.
-  // This can take 100-600 milliseconds! Uncomment whichever of the following you want to read
-  uint16_t x = tsl.getLuminosity(TSL2591_VISIBLE);
-  //uint16_t x = tsl.getLuminosity(TSL2591_FULLSPECTRUM);
-  //uint16_t x = tsl.getLuminosity(TSL2591_INFRARED);
-
-  Serial.print(F("[ ")); Serial.print(millis()); Serial.print(F(" ms ] "));
-  Serial.print(F("Luminosity: "));
-  Serial.println(x, DEC);
-}
-
-/**************************************************************************/
-/*
-    Show how to read IR and Full Spectrum at once and convert to lux
-*/
-/**************************************************************************/
-void advancedRead(void)
-{
-  // More advanced data read example. Read 32 bits with top 16 bits IR, bottom 16 bits full spectrum
-  // That way you can do whatever math and comparisons you want!
+// Realizar lectura del sensor
+// TODO convertir a mag/arcsec^2 y devolver el valor
+void TSL2591_read(void) {
   uint32_t lum = tsl.getFullLuminosity();
   uint16_t ir, full;
   ir = lum >> 16;
   full = lum & 0xFFFF;
-  Serial.print(F("[ ")); Serial.print(millis()); Serial.print(F(" ms ] "));
-  Serial.print(F("IR: ")); Serial.print(ir);  Serial.print(F("  "));
-  Serial.print(F("Full: ")); Serial.print(full); Serial.print(F("  "));
-  Serial.print(F("Visible: ")); Serial.print(full - ir); Serial.print(F("  "));
-  Serial.print(F("Lux: ")); Serial.println(tsl.calculateLux(full, ir), 6);
+  Serial.print(F("[ "));
+  Serial.print(millis());
+  Serial.print(F(" ms ] "));
+  Serial.print(F("IR: "));
+  Serial.print(ir);
+  Serial.print(F("  "));
+  Serial.print(F("Full: "));
+  Serial.print(full);
+  Serial.print(F("  "));
+  Serial.print(F("Visible: "));
+  Serial.print(full - ir);
+  Serial.print(F("  "));
+  Serial.print(F("Lux: "));
+  Serial.println(tsl.calculateLux(full, ir), 6);
 }
 
-/**************************************************************************/
-/*
-    Performs a read using the Adafruit Unified Sensor API.
-*/
-/**************************************************************************/
-void unifiedSensorAPIRead(void)
-{
-  /* Get a new sensor event */ 
-  sensors_event_t event;
-  tsl.getEvent(&event);
- 
-  /* Display the results (light is measured in lux) */
-  Serial.print(F("[ ")); Serial.print(event.timestamp); Serial.print(F(" ms ] "));
-  if ((event.light == 0) |
-      (event.light > 4294966000.0) | 
-      (event.light <-4294966000.0))
-  {
-    /* If event.light = 0 lux the sensor is probably saturated */
-    /* and no reliable data could be generated! */
-    /* if event.light is +/- 4294967040 there was a float over/underflow */
-    Serial.println(F("Datos inválidos (ajusta el gain o el timing)"));
-  }
-  else
-  {
-    Serial.print(event.light); Serial.println(F(" lux"));
-  }
-}
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Ready");
+  Serial.println("Iniciando...");
+
   // AHT20
   Wire.begin();
-  if (aht40.begin() == false) {
-    Serial.println("El módulo AHT20 no ha sido detectado. Por favor mira el cableado. Congelando");
-    while(true);
+  Serial.println(F("Buscando AHT20..."));
+  if (aht20.begin() == false) {
+    Serial.println("No se ha detectado ningún sensor AHT20. Congelando.");
+    while (true)
+      ;
   }
-  // TSL2591
-  Serial.println(F("Arrancando Adafruit TSL2591!"));
-  
-  if (tsl.begin()) 
-  {
-    Serial.println(F("El sensor TSL2591 fue encontrado"));
-  } 
-  else 
-  {
-    Serial.println(F("No se ha encontrado ningún sensor TSL2951...? Procediendo a congelar"));
-    while (1);
-  }
+  // TODO función similar a TSL2591_displayDetails() para AHT20?
+
   // BMP280
-  // Asegurarte de que el sensor BMP280 funcione con este código
-  //if (!bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID)) {
-  if (!bmp.begin()) 
-  {
-    Serial.println(F("No se ha podido encontrar un sensor BMP280, asegúrate que el cableado esté bien "
-                      "prueba otro pin!"));
-    while(1) delay(10); 
+  Serial.println(F("Buscando BMP280..."));
+  if (bmp.begin()) {
+    Serial.println(F("Sensor BMP280 encontrado."));
+  } else {
+    Serial.println(F("No se ha detectado ningun sensor BMP280. Congelando."));
+    while (true)
+      ;
   }
 
-  /* Las configuraciones por defecto de la datasheet*/
+  /* Las configuraciones por defecto del datasheet*/
   // Las he cogido del ejemplo bmp280_forced
+  // TODO revisar opciones (Sergio) y convertir esto en una función BMP280_configure(), como con TSL2591
   bmp.setSampling(Adafruit_BMP280::MODE_FORCED,     /* Operating Mode. */
                   Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
                   Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
                   Adafruit_BMP280::FILTER_X16,      /* Filtering. */
                   Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+
+  // TSL2591
+  Serial.println(F("Buscando TSL2591..."));
+  if (tsl.begin()) {
+    Serial.println(F("Sensor TSL2591 encontrado."));
+  } else {
+    Serial.println(F("No se ha detectado ningún sensor TSL2951. Congelando."));
+    while (true)
+      ;
+  }
+  TSL2591_displayDetails();
+  TSL2591_configure();
+
+  // TODO MLX90615
 }
 
 void loop() {
-  /*******************************************/
-  // TSL2591
-  // unifiedSensorAPIRead();
-  // advancedRead();
-  // simpleRead();
-  /*******************************************/
-  // Usando aht20
-  /***********************************
+  // AHT20
+  // TODO convertir esto en una función AHT20_read()
   float temperature = aht20.getTemperature(); // Aunque ya hay un temperature en el bmp, este es más fácil de usar
   float humidity = aht20.getHumidity(); // Este si o sí nos hace falta
   Serial.print("T: ");
@@ -248,10 +169,10 @@ void loop() {
   Serial.print(" C\t H: ");
   Serial.print(humidity, 2);
   Serial.println("% RH");
-  delay(2000);
-  ***********************************/
+
   // Usando BMP280
-    // must call this to wake sensor up and get new measurement data
+  // TODO convertir esto en una función BMP280_read()
+  // must call this to wake sensor up and get new measurement data
   // it blocks until measurement is complete
   if (bmp.takeForcedMeasurement()) {
     // can now print out the new measurements
@@ -272,10 +193,14 @@ void loop() {
     Serial.println(" m");
     /*************************************/
     Serial.println();
-    delay(2000);
   } else {
     // Mensaje de error
     Serial.println("Medición forzada ha fallado!");
   }
 
+  // TSL2591
+  TSL2591_read();
+
+  // TODO Determinar delay adecuado
+  delay(2000);
 }
